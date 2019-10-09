959528b4c9291d6549a63bf1efb9c5bb97ef85a1
EternalChildren <colin.zcn@outlook.com>
2019-10-09 13:53:18 +0800

    fix a render problem that animate view can't rerender by itself, update some types in index.d.ts, and remove the life cycle to be destroyed.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,27 +1,30 @@
-import React, { Component } from 'react'
+declare module "react-native-drag-sort" {
+  import React from "react";
 
-interface IProps{
-  dataSource: any[];
-  parentWidth: number;
-  childrenHeight: number;
-  childrenWidth: number;
+  export interface DragSortableViewProps<T> {
+    dataSource: T[];
+    parentWidth: number;
+    childrenHeight: number;
+    childrenWidth: number;
 
-  marginChildrenTop?: number;
-  marginChildrenBottom?: number;
-  marginChildrenLeft?: number;
-  marginChildrenRight?: number;
+    marginChildrenTop?: number;
+    marginChildrenBottom?: number;
+    marginChildrenLeft?: number;
+    marginChildrenRight?: number;
 
-  sortable?: boolean;
+    sortable?: boolean;
 
-  onClickItem?: (data: any[],item: any,index: number) => void;
-  onDragStart?: (fromIndex: number) => void;
-  onDragEnd?: (fromIndex: number,toIndex: number) => void;
-  onDataChange?: (data: any[]) => void;
-  renderItem: (item: any,index: number) => React.ReactElement<any>;
-  scaleStatus?: 'scale' | 'scaleX' |'scaleY';
-  fixedItems?: number[];
-  keyExtractor?: (item: any,index: number) => any,
-  delayLongPress?: number
+    onClickItem?: (data: T[],item: any,index: number) => void;
+    onDragStart?: (fromIndex: number) => void;
+    onDragEnd?: (fromIndex: number,toIndex: number) => void;
+    onDataChange?: (data: T[]) => void;
+    renderItem: (item: T,index: number) => React.ReactNode; 
+    scaleStatus?: 'scale' | 'scaleX' |'scaleY';
+    scaleNumber?: number;
+    fixedItems?: number[];
+    keyExtractor?: (item: T,index: number) => string | number,
+    delayLongPress?: number
+  }
+
+  export default class DragSortableView<T> extends React.Component<DragSortableViewProps<T>>{}
 }
-
-export default class DragSortableView extends Component<IProps>{}
